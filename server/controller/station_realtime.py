d06d4851dbadf006566265a45273075459583af7
evaseemeflt-t7920 <evaseemefly@126.com>
2024-03-06 15:59:26 +0800

    title:24-03-06 完成了整点实况与极值与maximum的单站与多站的查询接口

--- server/controller/station_realtime.py
+++ server/controller/station_realtime.py
@@ -2,7 +2,7 @@ from typing import List
 from datetime import datetime
 
 import arrow
-from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks, Request
+from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks, Request, Query
 
 from dao.station_surge import StationSurgeDao, StationSurgeExtremeDao
 from models.station import SurgePerclockDataModel, SurgePerclockExtremumDataModel
@@ -18,6 +18,15 @@ app = APIRouter()
 def get_one_station_perclock_byts(station_code: str, start_ts: int, end_ts: int):
     dao = StationSurgeDao()
     res = dao.get_station_realdata_list(station_code, start_ts, end_ts)
+    return res
+
+
+@app.get('/many/dtrange/perclock', response_model=List[StationSurgeSchema],
+         response_model_include=['station_code', 'issue_ts', 'surge', 'issue_dt'],
+         summary="获取指定站点集合指定起止时间(ts)的整点增水|整点总潮位(water level)数据集合")
+def get_many_station_perclock_byts(start_ts: int, end_ts: int, station_codes: List[str] = Query(None), ):
+    dao = StationSurgeDao()
+    res = dao.get_stations_realdata_list(station_codes, start_ts, end_ts)
     return res
 
 
@@ -39,6 +48,47 @@ def get_one_station_perclock_bydt(station_code: str, dt: str):
          response_model_include=['station_code', 'issue_ts', 'surge', 'issue_dt'],
          summary="获取指定站点指定时间范围内的极值集合")
 def get_one_station_extreme_byts(station_code: str, start_ts: int, end_ts: int):
+    dao = StationSurgeExtremeDao()
+    res = dao.get_station_extreme_list(station_code, start_ts, end_ts)
+    return res
+
+
+@app.get('/many/dtrange/extreme', response_model=List[StationSurgeSchema],
+         response_model_include=['station_code', 'issue_ts', 'surge', 'issue_dt'],
+         summary="获取指定站点指定时间范围内的极值集合")
+def get_many_station_extreme_byts(start_ts: int, end_ts: int, station_codes: List[str] = Query(None), ):
+    dao = StationSurgeExtremeDao()
+    res = dao.get_stations_extreme_list(station_codes, start_ts, end_ts)
+    return res
+
+
+@app.get('/one/dtrange/extreme/maximum', response_model=List[StationSurgeSchema],
+         response_model_include=['station_code', 'issue_ts', 'surge', 'issue_dt'],
+         summary="获取指定时间范围内的指定站点的极值")
+def get_one_station_maximum_byts(start_ts: int, end_ts: int, station_code: str):
+    dao = StationSurgeExtremeDao()
+    res = dao.get_one_extreme_maximum(station_code=station_code, start_ts=start_ts, end_ts=end_ts)
+    return res
+
+
+@app.get('/many/dtrange/extreme/maximum', response_model=List[StationSurgeSchema],
+         response_model_include=['station_code', 'issue_ts', 'surge', 'issue_dt'],
+         summary="获取指定时间范围内的指定站点的极值")
+def get_many_station_maximum_byts(start_ts: int, end_ts: int, station_codes: List[str] = Query(None), ):
+    dao = StationSurgeExtremeDao()
+    res = dao.get_many_extreme_maximum(station_codes=station_codes, start_ts=start_ts, end_ts=end_ts)
+    return res
+
+
+@app.get('/one/targetdt/extreme', response_model=List[StationSurgeSchema],
+         response_model_include=['station_code', 'issue_ts', 'surge', 'issue_dt'],
+         summary="获取指定站点指定日期(dt)的极值集合")
+def get_one_station_extreme_bydt(station_code: str, dt: str):
+    dt_str: str = dt
+    """YYYY-MM-DD格式的日期"""
+    # 03-15 -> start: 03-14 16:00
+    start_ts: int = arrow.get(dt_str, 'YYYY-MM-DD').shift(hours=-8).int_timestamp
+    end_ts: int = start_ts + 24 * 60 * 60
     dao = StationSurgeExtremeDao()
     res = dao.get_station_extreme_list(station_code, start_ts, end_ts)
     return res
--- server/dao/station_surge.py
+++ server/dao/station_surge.py
@@ -29,6 +29,23 @@ class StationSurgeDao(BaseDao):
                                                                                start_ts=start_ts, end_ts=end_ts)
         return list_surge
 
+    def get_stations_realdata_list(self, station_codes: List[str], start_ts: int, end_ts: int) -> List[
+        SurgeRealDataSchema]:
+        """
+            根据传入的海洋站 code 以及 起止时间获取时间范围内的 surge data 集合
+            起止时间不需要超过1个月
+        @param station_code:
+        @param gmt_start:
+        @param gmt_end:
+        @return:
+        """
+        # TODO:[*] 23-03-12 此处存在一个问题:当 起止时间不在一月(已在spider模块修改)
+        # 先判断是否需要联合表进行查询
+        list_surge: List[SurgePerclockDataModel] = self.get_stations_realdata_by_params(station_codes=station_codes,
+                                                                                        start_ts=start_ts,
+                                                                                        end_ts=end_ts)
+        return list_surge
+
     def get_realdata_by_params(self, **kwargs):
         """
             根据传入的动态参数查询实况
@@ -52,6 +69,32 @@ class StationSurgeDao(BaseDao):
         res = session.execute(stmt).scalars().all()
         return res
 
+    def get_stations_realdata_by_params(self, **kwargs):
+        """
+            获取对应 station_codes的实况
+        :param kwargs:
+        :return:
+        """
+        session: Session = self.db.session
+        station_codes: List[str] = kwargs.get('station_codes')
+        res: List[SurgePerclockDataModel] = []
+        """站点code"""
+        start_ts: int = kwargs.get('start_ts')
+        """起始ts"""
+        end_ts: int = kwargs.get('end_ts')
+        """结束ts(可空)"""
+        if end_ts is None:
+            # 未传入结束时间，按照start_ts+24h赋值
+            end_ts = start_ts + 24 * 60 * 60
+        for temp_code in station_codes:
+            stmt = select(SurgePerclockDataModel).where(SurgePerclockDataModel.station_code == temp_code,
+                                                        SurgePerclockDataModel.issue_ts >= start_ts,
+                                                        SurgePerclockDataModel.issue_ts <= end_ts).order_by(
+                SurgePerclockDataModel.issue_ts)
+            temp_res = session.execute(stmt).scalars().all()
+            res.extend(temp_res)
+        return res
+
 
 class StationSurgeExtremeDao(BaseDao):
     def get_station_extreme_list(self, station_code: str, start_ts: int, end_ts: int) -> List[
@@ -59,6 +102,40 @@ class StationSurgeExtremeDao(BaseDao):
         list_extreme: List[SurgePerclockExtremumDataModel] = self.get_extreme_byparams(station_code=station_code,
                                                                                        start_ts=start_ts, end_ts=end_ts)
         return list_extreme
+
+    def get_stations_extreme_list(self, station_codes: List[str], start_ts: int, end_ts: int) -> List[
+        SurgePerclockExtremumDataModel]:
+        list_extreme: List[SurgePerclockExtremumDataModel] = self.get_stations_extreme_byparams(
+            station_code=station_codes,
+            start_ts=start_ts, end_ts=end_ts)
+        return list_extreme
+
+    def get_stations_extreme_byparams(self, **kwargs):
+        """
+            获取站点集合对应时间内的极值集合
+        :param kwargs:
+        :return:
+        """
+        session: Session = self.db.session
+        station_codes: List[str] = kwargs.get('station_code')
+        """站点code"""
+        start_ts: int = kwargs.get('start_ts')
+        """起始ts"""
+        end_ts: int = kwargs.get('end_ts')
+        res: List[SurgePerclockExtremumDataModel] = []
+        """结束ts(可空)"""
+        if end_ts is None:
+            # 未传入结束时间，按照start_ts+24h赋值
+            end_ts = start_ts + 24 * 60 * 60
+        for temp_code in station_codes:
+            stmt = select(SurgePerclockExtremumDataModel).where(
+                SurgePerclockExtremumDataModel.station_code == temp_code,
+                SurgePerclockExtremumDataModel.issue_ts >= start_ts,
+                SurgePerclockExtremumDataModel.issue_ts <= end_ts).order_by(
+                SurgePerclockExtremumDataModel.issue_ts)
+            temp_res = session.execute(stmt).scalars().all()
+            res.extend(temp_res)
+        return res
 
     def get_extreme_byparams(self, **kwargs):
         session: Session = self.db.session
@@ -76,4 +153,53 @@ class StationSurgeExtremeDao(BaseDao):
                                                             SurgePerclockExtremumDataModel.issue_ts <= end_ts).order_by(
             SurgePerclockExtremumDataModel.issue_ts)
         res = session.execute(stmt).scalars().all()
+        return res
+
+    def get_one_extreme_maximum(self, **kwargs):
+        session: Session = self.db.session
+        station_code: str = kwargs.get('station_code')
+        """站点code"""
+        start_ts: int = kwargs.get('start_ts')
+        """起始ts"""
+        end_ts: int = kwargs.get('end_ts')
+        """结束ts(可空)"""
+        if end_ts is None:
+            # 未传入结束时间，按照start_ts+24h赋值
+            end_ts = start_ts + 24 * 60 * 60
+        stmt = select(SurgePerclockExtremumDataModel).where(
+            SurgePerclockExtremumDataModel.station_code == station_code,
+            SurgePerclockExtremumDataModel.issue_ts >= start_ts,
+            SurgePerclockExtremumDataModel.issue_ts <= end_ts)
+        sub_stmt = select(func.max(SurgePerclockExtremumDataModel.surge)).where(
+            SurgePerclockExtremumDataModel.station_code == station_code,
+            SurgePerclockExtremumDataModel.issue_ts >= start_ts,
+            SurgePerclockExtremumDataModel.issue_ts <= end_ts)
+        stmt = stmt.where(SurgePerclockExtremumDataModel.surge == sub_stmt)
+        res = session.execute(stmt).scalars().all()
+        return res
+
+    def get_many_extreme_maximum(self, **kwargs):
+        session: Session = self.db.session
+        station_codes: str = kwargs.get('station_codes')
+        """站点code"""
+        start_ts: int = kwargs.get('start_ts')
+        """起始ts"""
+        end_ts: int = kwargs.get('end_ts')
+        res: List[SurgePerclockExtremumDataModel] = []
+        """结束ts(可空)"""
+        if end_ts is None:
+            # 未传入结束时间，按照start_ts+24h赋值
+            end_ts = start_ts + 24 * 60 * 60
+        for temp_code in station_codes:
+            stmt = select(SurgePerclockExtremumDataModel).where(
+                SurgePerclockExtremumDataModel.station_code == temp_code,
+                SurgePerclockExtremumDataModel.issue_ts >= start_ts,
+                SurgePerclockExtremumDataModel.issue_ts <= end_ts)
+            sub_stmt = select(func.max(SurgePerclockExtremumDataModel.surge)).where(
+                SurgePerclockExtremumDataModel.station_code == temp_code,
+                SurgePerclockExtremumDataModel.issue_ts >= start_ts,
+                SurgePerclockExtremumDataModel.issue_ts <= end_ts)
+            stmt = stmt.where(SurgePerclockExtremumDataModel.surge == sub_stmt)
+            temp_res = session.execute(stmt).scalars().all()
+            res.extend(temp_res)
         return res
